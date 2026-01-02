68eaedd5e5fbf5c1eef56b39e22e7246f314af53
Pikacnu <a13456123456@gmail.com>
2026-01-02 18:49:02 +0800

    refactor: remove commented-out bot scanning and execution code

--- apps/bots/src/index.ts
+++ apps/bots/src/index.ts
@@ -1,4 +1,39 @@
-// Do something
-export const doSomething = () => {
-  console.log('Doing something...');
-};
+// import { Glob } from 'bun';
+// // scan Files
+// const scanner = new Glob('*.ts');
+// const modulesPath: string[] = [];
+// for await (const f of scanner.scan('/src/functions')) {
+//   modulesPath.push(f);
+// }
+
+// // import all modules
+// const moduleFunctions = await Promise.all(
+//   modulesPath.map(async (modulePath) => {
+//     const module = await import(modulePath);
+//     return module.default;
+//   }),
+// );
+
+// const intervals: NodeJS.Timeout[] = [];
+
+// for (const getBotFn of moduleFunctions) {
+//   const { interval, fn } = getBotFn();
+//   if (!Number(interval) || isNaN(Number(interval)) || !fn) continue;
+//   if (typeof fn !== 'function') continue;
+//   const intervalId = setInterval(async () => {
+//     try {
+//       await fn();
+//     } catch (error) {
+//       console.error('Error executing bot function:', error);
+//     }
+//   }, interval);
+//   intervals.push(intervalId);
+// }
+
+// process.on('SIGINT', async () => {
+//   console.log('Gracefully shutting down bots...');
+//   for (const intervalId of intervals) {
+//     clearInterval(intervalId);
+//   }
+//   process.exit();
+// });
